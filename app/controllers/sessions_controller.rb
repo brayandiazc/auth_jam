12effd68ade8494f24b0153b284faed64b1f7837
Brayan Diaz C <brayandiazc@gmail.com>
2022-05-20 20:46:21 -0500

    last commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Loguead Correctamente'
+    else
+      flash.now[:notice] = 'Email o password invalida'
+      render action: :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Signed out successfully.'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :sessions, only: [:new, :create, :destroy]
+  root "home#index"
 end
