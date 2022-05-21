22c4850afd222cf1d9325ac2cb700488c3cc0e1a
Brayan Diaz C <brayandiazc@gmail.com>
2022-05-20 19:55:24 -0500

    feat: Sessions and home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
